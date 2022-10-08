e9f470e56f6a1b65d7deab3f37af18a8906ce7e1
Mettralla <daxter0493@gmail.com>
2022-10-08 17:11:52 -0300

    Adds the user create endpoint

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,13 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
+  before { @first_user = create(:user) }
+
   describe "GET /show" do
-    before { @first_user = create(:user) }
     it "should show a user" do
       get '/api/v1/users/1'
       expect(response).to have_http_status(:success)
       json_response = JSON.parse(response.body)
       expect(@first_user.email).to eq(json_response['email'])
+    end
+  end
+
+  describe "POST /users" do
+    it "should create user" do
+      expect {
+        post '/api/v1/users', params: {
+          user: {
+            email: 'test@test',
+            password: '123456'
+          }
+        }
+      }.to change { User.count }.from(1).to(2)
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'should not create user with taken email' do
+      expect {
+        post '/api/v1/users', params: {
+          user: {
+            email: 'one@one.org',
+            password: 'trustno1'
+          }
+        }
+      }.not_to change { User.count }
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
